79046ce74b7330ef113c9033e153bed42884ae16
Betsu <nehmiabm@gmail.com>
2016-08-23 03:22:31 -0500

    bow analysis program

---
+++ BOWAnalysis/HiveConnection.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Hadoop.Hive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BOWAnalysis
+{
+    public class SampleHiveConnection : HiveConnection
+    {
+       
+        public SampleHiveConnection(Uri uri, string username, string password, string asvAccount, string asvKey) 
+            : base(uri, username,password,asvAccount,asvKey)
+        {
+        }
+        public HiveTable<DocumentInfo> DocumentTable
+        {
+            get { return this.GetTable<DocumentInfo>("docTable"); }
+        }
+        public HiveTable<WordInfo> WordTable
+        {
+            get { return this.GetTable<WordInfo>("vocabTable"); }
+        }
+        public class DocumentInfo : HiveRow
+        {
+            public int docID { get; set; }
+            public int wordID { get; set; }
+            public int count { get; set; }
+        }
+        public class WordInfo : HiveRow
+        {
+            public int wordID { get; set; }
+            public string word { get; set; }
+        }
+    } 
+}
--- BOWAnalysis/Program.cs
+++ BOWAnalysis/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Hadoop.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,35 @@ namespace BOWAnalysis
     {
         static void Main(string[] args)
         {
+            var asvAccount = "adminds.blob.core.windows.net";
+            var asvKey = "6nARLyzpNEYJRqZKpohMnCK3qVfFEKNHxhrS20SR57W6TjoS6CvQcIYxGdcRtNS4zeKP9+1zYkUW3tz1QfIMSQ==";
+
+            var hive = new SampleHiveConnection(new Uri("https://myhdicluster.azurehdinsight.net"),
+        "admin", "Password@123",
+        asvAccount,
+         asvKey);
+
+         //   var r = from w in hive.WordTable where w.wordID == 1 select w;
+
+            var res = (from doc in hive.DocumentTable
+                       join w in hive.WordTable on doc.wordID equals w.wordID
+                       group new { doc, w } by w.word
+                      into selection
+                       select new
+                       {
+                           word = selection.Key,
+                           freq = selection.Sum(x => x.doc.count)
+                       }).OrderByDescending(e => e.freq).Take(10);
+
+            var result = hive.ExecuteQuery(res.ToString());
+            result.Wait();
+            
+            Console.WriteLine("The results are: {0}", result.Result);
+            Console.ReadKey();
+
         }
+
+
+
     }
 }
